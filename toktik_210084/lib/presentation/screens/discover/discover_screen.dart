4f4d2be649ec453a93cbc100cfcfe0217a171f43
Janeth <210084@utxicotepec.edu.mx>
2024-12-02 21:43:37 -0600

    Practica 25
    Creacrión del archivo, human formats, fullscreen, modificacion del video buttons y video scrollable

--- toktik_210084/lib/presentation/screens/discover/discover_screen.dart
+++ toktik_210084/lib/presentation/screens/discover/discover_screen.dart
@@ -1,35 +1,35 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:toktik_210084/presentation/providers/discover_provider.dart';
-// import 'package:toktik_210084/presentation/widgets/shared/video_scrollable_view.dart';
-
-// class DiscoverScreen extends StatelessWidget {
-//   const DiscoverScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-    
-//     final discoverProvider = context.watch<DiscoverProvider>();
-
-//     return  Scaffold(
-//       body: discoverProvider.initialLoading
-//       ? const Center( child: CircularProgressIndicator())
-//       : VideoScrollableView(videos: discoverProvider.videos)
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik_210084/presentation/providers/discover_provider.dart';
+import 'package:toktik_210084/presentation/widgets/shared/video_scrollable_view.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('DiscoverScreen'),
-      ),
+    
+    final discoverProvider = context.watch<DiscoverProvider>();
+
+    return  Scaffold(
+      body: discoverProvider.initialLoading
+      ? const Center( child: CircularProgressIndicator())
+      : VideoScrollableView(videos: discoverProvider.videos)
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// class DiscoverScreen extends StatelessWidget {
+//   const DiscoverScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       body: Center(
+//         child: Text('DiscoverScreen'),
+//       ),
+//     );
+//   }
+// }
--- toktik_210084/lib/presentation/widgets/shared/video_buttons.dart
+++ toktik_210084/lib/presentation/widgets/shared/video_buttons.dart
@@ -1,42 +1,57 @@
-// import 'package:flutter/material.dart';
-// import 'package:toktik_210084/domain/entities/video_post.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:toktik_210084/config/helpers/human_formats.dart';
+import 'package:toktik_210084/domain/entities/video_post.dart';
 
-// class VideoButtons extends StatelessWidget {
-//   final VideoPost video;
-//   const  VideoButtons({
-//     super.key,
-//     required this.video
-//   });
+class VideoButtons extends StatelessWidget {
+  final VideoPost video;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         _CustomIconButton(value: video.likes, iconData: Icons.favorite, color: Colors.red)
-//       ],
-//     );
-//   }
-// }
+  const  VideoButtons({
+    super.key,
+    required this.video
+  });
 
-// class _CustomIconButton extends StatelessWidget{
-//   final int value;
-//   final IconData iconData;
-//   final Color? color;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //_CustomIconButton(value: video.likes, iconColor: Colors.red, iconData: IconData.favorite),
+        _CustomIconButton(value: video.likes, iconData: Icons.favorite, iconColor: Colors.red),
+        _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye_outlined),
 
-//   const _CustomIconButton({
-//     required this.value,
-//     required this.iconData,
-//     required this.color
-//     });
+        //const SizeBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 3),
+          child: const _CustomIconButton(value:0, iconData: Icons.play_circle_outline),
+        )
+        
+      ],
+    );
+  }
+}
 
-//     @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         IconButton(onPressed: () {}, icon: Icon(Icons.favorite, color: Colors.red,)),
+class _CustomIconButton extends StatelessWidget{
+  final int value;
+  final IconData iconData;
+  final Color? color;
 
-//         Text('$value'),
-//       ],
-//     );
-//   }
-// }
+  const _CustomIconButton({
+    required this.value,
+    required this.iconData,
+    iconColor
+    }): color = iconColor ?? Colors.white;
+
+    @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(onPressed: () {}, icon: Icon(iconData, color: color, size: 30)),
+
+        if(value > 0)
+        Text('${HumanFormats.humanReadbleNumber(value.toDouble())}'),
+      ],
+    );
+  }
+}
