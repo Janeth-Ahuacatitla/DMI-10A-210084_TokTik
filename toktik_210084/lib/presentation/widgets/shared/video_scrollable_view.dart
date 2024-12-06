ebc50cbcc911517178ff265ae236d4361bce61b1
Janeth-Ahuacatitla <210084@utxicotepec.edu.mx>
2024-12-06 10:47:39 -0600

    Practica 25
    Modificacion del archivo fullscreen player

--- toktik_210084/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ toktik_210084/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:toktik_210084/domain/entities/video_post.dart';
-import 'package:toktik_210084/presentation/widgets/shared/video/fullscreen_player.dart';
 import 'package:toktik_210084/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik_210084/presentation/widgets/shared/video/fullscreen_player.dart';
 
-class VideoScrollableView extends StatelessWidget{
+
+class VideoScrollableView extends StatelessWidget {
+  
   final List<VideoPost> videos;
-
+  
   const VideoScrollableView({
-    super.key,
+    super.key, 
     required this.videos
   });
 
@@ -17,31 +19,81 @@ class VideoScrollableView extends StatelessWidget{
       scrollDirection: Axis.vertical,
       physics: const BouncingScrollPhysics(),
       itemCount: videos.length,
-      itemBuilder: (context, index){
+      itemBuilder: (context, index) {
         final VideoPost videoPost = videos[index];
 
         return Stack(
           children: [
-
+            // Video Player + gradiente
             SizedBox.expand(
-              child: FullscreenPlayer(
+              child: FullScreenPlayer(
                 caption: videoPost.caption,
                 videoUrl: videoPost.videoUrl,
-              ),
+              )
             ),
-            //video Player + gradiente 
+
+            // Botones
             Positioned(
               bottom: 40,
               right: 20,
-              child: VideoButtons(video: videoPost),
+              child: VideoButtons(video: videoPost)
             ),
-            //Botones
+            
           ],
         );
+
       },
     );
   }
 }
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:toktik_210084/domain/entities/video_post.dart';
+// import 'package:toktik_210084/presentation/widgets/shared/video/fullscreen_player.dart';
+// import 'package:toktik_210084/presentation/widgets/shared/video_buttons.dart';
+
+// class VideoScrollableView extends StatelessWidget{
+//   final List<VideoPost> videos;
+
+//   const VideoScrollableView({
+//     super.key,
+//     required this.videos
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return PageView.builder(
+//       scrollDirection: Axis.vertical,
+//       physics: const BouncingScrollPhysics(),
+//       itemCount: videos.length,
+//       itemBuilder: (context, index){
+//         final VideoPost videoPost = videos[index];
+
+//         return Stack(
+//           children: [
+
+//             SizedBox.expand(
+//               child: FullscreenPlayer(
+//                 caption: videoPost.caption,
+//                 videoUrl: videoPost.videoUrl,
+//               ),
+//             ),
+//             //video Player + gradiente 
+//             Positioned(
+//               bottom: 40,
+//               right: 20,
+//               child: VideoButtons(video: videoPost),
+//             ),
+//             //Botones
+//           ],
+//         );
+//       },
+//     );
+//   }
+// }
 
 // class Positioned extends StatelessWidget {
 //   const Positioned({
